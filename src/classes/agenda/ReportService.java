132b40dd82f647c7eee3b19ca038ca9034bbefbc
Daniel <dmribeiro@unifesp.br>
2026-05-11 22:03:38 -0300

    Add case treatment for event types in observers

--- src/classes/agenda/ReportService.java
+++ src/classes/agenda/ReportService.java
@@ -5,15 +5,28 @@ import java.util.Collections;
 import java.util.List;
 
 import src.classes.reservation.Reservation;
+import src.classes.reservation.ReservationEvent;
 import src.observers.AgendaObserver;
 
 public class ReportService implements AgendaObserver {
     private final List<Reservation> log = new ArrayList<>();
 
     @Override
-    public void update(Reservation reservation) {
-        log.add(reservation);
-        System.out.println("[ReportService] Logged reservation: " + reservation);
+    public void update(ReservationEvent event) {
+        switch (event.getEventType()) {
+            case CREATED:
+                System.out.println("[ReportService] New reservation created: " + event.getReservation());
+                log.add(event.getReservation()); // Adds created reservation to log
+                break;
+            case CANCELLED:
+                System.out.println("[ReportService] Reservation cancelled: " + event.getReservation());
+                log.add(event.getReservation()); // Adds cancelled reservation to log
+                break;
+            case MODIFIED:
+                System.out.println("[ReportService] Reservation modified: " + event.getReservation());
+                log.add(event.getReservation()); // Adds modified reservation to log
+                break;
+        }
     }
 
     public void printReport() {
--- src/classes/agenda/UserNotifier.java
+++ src/classes/agenda/UserNotifier.java
@@ -1,6 +1,7 @@
 package src.classes.agenda;
 
 import src.classes.reservation.Reservation;
+import src.classes.reservation.ReservationEvent;
 import src.classes.user.User;
 import src.observers.AgendaObserver;
 
@@ -12,8 +13,17 @@ public class UserNotifier implements AgendaObserver {
     }
 
     @Override
-    public void update(Reservation reservation) {
-        System.out.println("Notification to " + user.getName() + 
-        ": reservation confirmed for " + reservation.toString());
+    public void update(ReservationEvent event) {
+        switch (event.getEventType()) {
+            case CREATED:
+                System.out.println("[UserNotifier] Hello " + user.getName() + ", your reservation has been created: " + event.getReservation());
+                break;
+            case CANCELLED:
+                System.out.println("[UserNotifier] Hello " + user.getName() + ", your reservation has been cancelled: " + event.getReservation());
+                break;
+            case MODIFIED:
+                System.out.println("[UserNotifier] Hello " + user.getName() + ", your reservation has been modified: " + event.getReservation());
+                break;
+        }
     }
 }
