6fd5004511303f85e20f2bdd3b1c28419999ef97
Daniel <dmribeiro@unifesp.br>
2026-05-11 22:39:09 -0300

    Fix

--- src/factorys/ClassroomFactory.java
+++ src/factorys/ClassroomFactory.java
@@ -3,18 +3,28 @@ package src.factorys;
 import src.classes.classrooms.LabClassroom;
 import src.classes.classrooms.LectureClassroom;
 import src.classes.classrooms.StudyClassroom;
-import src.interfaces.Classroom;
+import src.classes.classroom.Classroom;
+import src.policies.*;
 
 public class ClassroomFactory {
     public enum ClassType{
         LAB, STUDY, LECTURE
     }
 
-    public static Classroom create(ClassType type){
+    public static Classroom create(ClassType type, int id, int capacity){
         switch (type){
-            case LECTURE: return new LectureClassroom();
-            case STUDY: return new StudyClassroom();
-            case LAB: return new LabClassroom();
+            case LECTURE: return new LectureClassroom(id, capacity);
+            case STUDY: return new StudyClassroom(id, capacity);
+            case LAB: return new LabClassroom(id, capacity);
+            default: throw new IllegalArgumentException("Unknown type: " + type);
+        }
+    }
+
+    public static Classroom create(ClassType type, int id, int capacity, ReservationPolicy policy){
+        switch (type){
+            case LECTURE: return new LectureClassroom(id, capacity, policy);
+            case STUDY: return new StudyClassroom(id, capacity, policy);
+            case LAB: return new LabClassroom(id, capacity, policy);
             default: throw new IllegalArgumentException("Unknown type: " + type);
         }
     }
