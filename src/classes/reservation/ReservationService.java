f2dfd522f4aa0ebf0777827ee8efa95011247e88
Daniel <dmribeiro24@gmail.com>
2026-05-11 21:05:10 -0300

    Add ReservationService class

---
+++ src/classes/reservation/ReservationService.java
@@ -0,0 +1,19 @@
+package src.classes.reservation;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import src.interfaces.Classroom;
+import src.classes.user.User;
+
+public class ReservationService {
+
+    public void makeReservation(Classroom classroom, User user, DayOfWeek day, LocalTime startTime, LocalTime endTime) {
+        Reservation newRes = new Reservation(day, startTime, endTime);
+        classroom.reserve();
+    }
+
+}
