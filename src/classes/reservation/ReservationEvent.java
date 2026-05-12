65ecee05e5480174ccd46a0da622af7de19f6e67
Daniel <dmribeiro@unifesp.br>
2026-05-12 18:33:21 -0300

    Add ReservationEvent class

---
+++ src/classes/reservation/ReservationEvent.java
@@ -0,0 +1,18 @@
+package src.classes.reservation;
+
+enum ReservationEventType {
+    CREATED, MODIFIED, CANCELLED
+}
+
+public class ReservationEvent {
+    private final ReservationEventType type;
+    private final Reservation reservation;
+
+    public ReservationEvent(ReservationEventType type, Reservation reservation) {
+        this.type = type;
+        this.reservation = reservation;
+    }
+
+    public ReservationEventType getType() { return type; }
+    public Reservation getReservation() { return reservation; }
+}
